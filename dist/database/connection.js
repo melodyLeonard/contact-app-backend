665984be37034a6cdf5b82fd500bc6e76b10f36f
melodyleonardv <m.oparaugo@vetifly.com>
2022-07-26 12:47:30 +0100

    added db connections

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -4,9 +4,9 @@ exports.AppDataSource = void 0;
 var typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    database: "contact",
-    username: "postgres",
-    password: "postgres",
+    database: "d2pu4r2hf51vo9",
+    username: "kumftsbsclqfmz",
+    password: "90f87aeb11dc269ecd9c24c0a2e46fed3eef7a0cc14a6d0f705150957879349a",
     port: 5432,
     host: "localhost",
     entities: ["./src/entities/*{.ts,.js}"],
@@ -14,7 +14,7 @@ exports.AppDataSource = new typeorm_1.DataSource({
     migrations: [],
     synchronize: true,
     logger: "advanced-console",
-    logging: "all",
+    logging: false,
     dropSchema: false,
     cache: true,
 });
