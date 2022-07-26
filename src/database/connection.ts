8d0a5d1cfb3f351183cea701e0afa55bed51e216
melodyleonardv <m.oparaugo@vetifly.com>
2022-07-26 13:42:45 +0100

    added db host

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -6,7 +6,7 @@ import {DataSource} from 'typeorm';
       username: "kumftsbsclqfmz", // fill this with your username
       password: "90f87aeb11dc269ecd9c24c0a2e46fed3eef7a0cc14a6d0f705150957879349a", // and password
       port: 5432, // and port
-      host: "localhost", // and host
+      host: "kumftsbsclqfmz:90f87aeb11dc269ecd9c24c0a2e46fed3eef7a0cc14a6d0f705150957879349a@ec2-3-217-14-181.compute-1.amazonaws.com", // and host
       entities: [__dirname + "/../entities/*{.ts, .js}"],
       subscribers: [],
       migrations: [],
